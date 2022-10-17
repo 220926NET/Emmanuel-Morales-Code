c8b5a4c2c459e54dea1010fabe647a9d265383bb
Emmanuel <emmanuelmartinez122623@gmail.com>
2022-10-16 18:00:11 -0700

    added comments

--- project-1/consoleApp/TicketTrackerApp/Services/InputValidator.cs
+++ project-1/consoleApp/TicketTrackerApp/Services/InputValidator.cs
@@ -1,47 +1,65 @@
 using System.Text;
 
+
+/// <summary>
+/// Class <c>InputValidator</c> deals with validating strings 
+/// </summary>
 public class InputValidator
 {
 
-    //TODO write tests to verify functionality 
+    
+    
+    
+    
 
-    public bool IsValidUserName(string? userName)
+    /// <summary>
+    /// <method><c>IsValidUserName<c></method> 
+    /// <para>
+    /// This method validates user name by checking if the string is Null or empty
+    /// </para> 
+    ///<returns>
+    /// True or false
+    /// </returns> 
+    /// </summary>
+    public bool isValidName(String? name)
     {
-        if (string.IsNullOrEmpty(userName))
+        if (string.IsNullOrEmpty(name))
         {
 
             return false;
         }
 
         return true;
-
     }
 
-
-    public bool isValidName(String str)
-    {
-        if (string.IsNullOrEmpty(str))
-        {
-
-            return false;
-        }
-
-        return true;
-    }
+    /// <summary>
+    /// <method><c>IsValidPassword<c></method> 
+    /// <para>
+    /// This method validates user password by checking if it contains at least one digit and is not null or empty, 
+    /// 
+    /// </para> 
+    ///<returns>
+    /// True or false
+    /// </returns> 
+    /// </summary>
     public bool IsValidPassword(string? password)
     {
 
         bool containsDigit = false;
+        if (string.IsNullOrEmpty(password))
+        {
+            return false;
+        }
         for (int i = 0; i < 10; i++)
         {
-            if (password.Contains(i.ToString()))
+            if (password!.Contains(i.ToString()))
             {
                 containsDigit = true;
 
             }
         }
 
-        if (password.Trim().Length > 16)
+        if (password!.Trim().Length > 16)
         {
             return false;
 
@@ -51,10 +69,7 @@ public class InputValidator
         {
             return false;
         }
-        if (ContainsNull(password))
-        {
-            return false;
-        }
+        
         if (!containsDigit)
         {
             return false;
@@ -62,31 +77,41 @@ public class InputValidator
         return true;
     }
 
-    public InputResponse IsValidOptionInput(string? input, int maxNumInput)
+    /// <summary>
+    /// <method><c>IsValidOptionInput<c></method> 
+    /// <para>
+    /// This method validates user options by ensuring they stay within 0 (exclusive) and maxNumInput(Inclusive)
+    /// </para> 
+    ///<returns>
+    /// True or false
+    /// </returns> 
+    /// </summary>
+    public bool IsValidOptionInput(string? input, int maxNumInput = 3)
     {
-        InputResponse inputResponse = new InputResponse();
         int num;
         bool inputIsNum = int.TryParse(input, out num);
-        if (!inputIsNum || num > maxNumInput || num < 0)
+        if (!inputIsNum || num > maxNumInput || num < 1)
         {
-
-            inputResponse.success = false;
-            inputResponse.messages.Add($"Please type a number between 1 and {maxNumInput}");
-            return inputResponse;
+            return false;
         }
         else
         {
-            inputResponse.success = true;
-            return inputResponse;
+            return true;
         }
-
-
     }
 
+    /// <summary>
+    /// <method><c>IsValidDescriptionAndAmount<c></method> 
+    /// <para>
+    /// This method validates a amount string and description str by ensuring 
+    /// that amount str can be converted into a decimal and description string is not null 
+    /// </para> 
+    ///<returns>
+    /// True or false
+    /// </returns> 
+    /// </summary>
     public bool IsValidDescriptionAndAmount(string? description, string? amountStr)
     {
-
-
 
         if (!IsValidAmount(amountStr))
         {
@@ -105,15 +130,55 @@ public class InputValidator
         return true;
     }
 
+    /// <summary>
+    /// <method><c>isValidTicketDescription<c></method> 
+    /// <para>
+    /// This method validates a amount string and description str by ensuring 
+    /// that amount str can be converted into a decimal and description string is not null 
+    /// </para> 
+    ///<returns>
+    /// True or false
+    /// </returns> 
+    /// </summary>
+    public bool isValidTicketDescription(string? description){
+        // TODO add more validation 
+        if (String.IsNullOrEmpty(description))
+        {
+
+            return false;
+        }
+
+        return true;
+    }
+    
+    /// <summary>
+    /// <method><c>IsValidAmount<c></method> 
+    /// <para>
+    /// This method validates a amount string a by testing if amountStr can be converted into a decimal
+    /// </para> 
+    ///<returns>
+    /// True or false
+    /// </returns> 
+    /// </summary>
     public bool IsValidAmount(string? amountStr)
     {
-        if (ContainsNull(amountStr)) return false;
-        int amountInt = 0;
-        bool isNum = int.TryParse(amountStr, out amountInt);
+        if (String.IsNullOrEmpty(amountStr)) return false;
+        decimal amountInt = 0;
+        bool isNum = decimal.TryParse(amountStr, out amountInt);
         if (!isNum) return false;
         return true;
     }
 
+    /// <summary>
+    /// <method><c>IsValidAmount<c></method> 
+    /// <para>
+    /// This method validates an id and decision by ensuring that 
+    /// id can be converted into an integer and decision is equal to ethier = "approve" or "deny"
+    /// </para> 
+    ///<returns>
+    /// True or false
+    /// </returns> 
+    /// </summary>
     public bool IsValidManagerChoice(int? id, string? decision)
     {
 
@@ -130,19 +195,8 @@ public class InputValidator
         {
             return false;
         }
-        // Todo check that there exists at least two substrings 
+       
         return true;
 
-
     }
-
-    public bool ContainsNull(string? text)
-    {
-        if (string.IsNullOrWhiteSpace(text))
-        {
-            return true;
-        }
-        return false;
-    }
-
 }
