318f62eaaa62aa246f03d47c2899cc2beb89041c
Emmanuel <emmanuelmartinez122623@gmail.com>
2022-10-19 14:52:18 -0700

    Add Register Services

--- api-using-adonet/EmployeeReApiAdo/Controllers/RegisterController.cs
+++ api-using-adonet/EmployeeReApiAdo/Controllers/RegisterController.cs
@@ -7,13 +7,19 @@ public class RegisterController : ControllerBase {
 
     private readonly DbContext _dbContext = new DbContext(); 
     
+    private IRegisterService _registerService; 
+    public RegisterController(IRegisterService registerService)
+    {
+        _registerService = registerService; 
+        
+    }
 
     [HttpPost]
      public ActionResult<ServiceResponse<string>> SignUp(Employee employee)
         {
             ServiceResponse<string> response = new ServiceResponse<string>();
 
-            response = _dbContext.CreateUser(employee); 
+            response = _registerService.SignUp(employee); 
 
             return response; 
             
