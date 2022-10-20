c74faa159e204cc68ca293543b439f60094f652c
Emmanuel <emmanuelmartinez122623@gmail.com>
2022-10-20 16:38:16 -0700

    Update login model

---
+++ api-using-adonet/EmployeeReApiAdo/Models/Image.cs
@@ -0,0 +1,6 @@
+public class Image {
+
+    public int Employeeid {get; set;}
+    public string Name {get; set;}
+    public string Photo {get; set;}
+}
--- api-using-adonet/EmployeeReApiAdo/Models/Login.cs
+++ api-using-adonet/EmployeeReApiAdo/Models/Login.cs
@@ -1,6 +1,6 @@
 public class Login {
 
-    public int id {get; set; } = 0; 
+    
     public string UserName {get; set;} = string.Empty;
 
     public string Password {get; set;} = string.Empty; 
