214c7dff1c365d34bbcf7f34702178a1e4bba9ae
Emmanuel <emmanuelmartinez122623@gmail.com>
2022-10-20 22:23:10 -0700

    Create dto for employee details

---
+++ api-using-adonet/EmployeeReApiAdo/DTOs/EmployeeDetailsDto.cs
@@ -0,0 +1,15 @@
+
+public class EmployeeDetailsDto {
+    
+    public int EmployeeId {get; set;}
+
+    public string Address {get; set; }
+
+    public string State {get; set;}
+
+    public int ZipCode {get; set;}
+
+    public int MobileNumber {get; set;}
+
+
+}
